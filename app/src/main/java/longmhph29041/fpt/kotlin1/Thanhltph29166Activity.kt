f3fc9995623d64de5735b9d41d1700bed60a81e4
LE TIEN THANH <thanhltph29166@fpt.edu.vn>
2023-03-12 12:39:31 +0700

    acitivy2_thanhltph29166_12/03

---
+++ app/src/main/java/longmhph29041/fpt/kotlin1/Thanhltph29166Activity.kt
@@ -0,0 +1,12 @@
+package longmhph29041.fpt.kotlin1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Thanhltph29166Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thanhltph29166)
+        ///thanh mount
+    }
+}

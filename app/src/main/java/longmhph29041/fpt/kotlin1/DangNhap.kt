13c21374784df95f95a471431192d0db3c50a408
long2 <longmhph29041@fpt.edu.vn>
2023-03-12 11:24:05 +0700

    PH29041 date time

--- app/src/main/java/longmhph29041/fpt/kotlin1/DangNhap.kt
+++ app/src/main/java/longmhph29041/fpt/kotlin1/DangNhap.kt
@@ -23,6 +23,8 @@ class DangNhap : AppCompatActivity() {
                 Toast.makeText(this,"Đăng nhập thành công",Toast.LENGTH_LONG).show()
                 //Helooo
                 //helooo broduct
+
+                //11h 23p ngày 12.3.2023
             }
         }
     }
